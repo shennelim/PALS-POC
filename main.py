12007cf21164c0eeaa3aec3bfb87b06ec9d0089e
kelitan <contact.kellietan@gmail.com>
2024-11-06 11:53:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,12 +28,16 @@ documents_loaded = load_files()
 #Disclaimer
 with st.expander("DISCLAIMER"):
     st.markdown("""
-                **IMPORTANT NOTICE**: This web application is developed as a proof-of-concept prototype. \
+                **IMPORTANT NOTICE**: 
+                
+                This web application is developed as a proof-of-concept prototype. \
                 The information provided here is **NOT intended for actual usage** and should not be relied upon \
                 for making any decisions, especially those related to financial, legal, or healthcare matters. \
+
                 
                 **Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. \
                 You assume full responsibility for how you use any generated output.** \
+
                 
                 Always consult with qualified professionals for accurate and personalized advice.
                 """)
