76c5290e5827aafb1966209791c90a891fa1e41c
kelitan <contact.kellietan@gmail.com>
2024-11-06 11:54:23 +0800

    Update 3_About_Us.py

--- pages/3_About_Us.py
+++ pages/3_About_Us.py
@@ -7,6 +7,23 @@ st.set_page_config(
 )
 # endregion <--------- Streamlit App Configuration --------->
 
+#Disclaimer
+with st.expander("DISCLAIMER"):
+    st.markdown("""
+                **IMPORTANT NOTICE**: 
+                
+                This web application is developed as a proof-of-concept prototype. \
+                The information provided here is **NOT intended for actual usage** and should not be relied upon \
+                for making any decisions, especially those related to financial, legal, or healthcare matters. \
+
+                
+                **Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. \
+                You assume full responsibility for how you use any generated output.** \
+
+                
+                Always consult with qualified professionals for accurate and personalized advice.
+                """)
+
 # Title
 st.markdown("# About Us")
 
